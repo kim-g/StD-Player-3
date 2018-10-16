5db63db17425a9e01fce885df62321b9875d8afb
Григорий Ким <kim-g@yandex.ru>
2018-10-16 21:19:52 +0500

    РЕЛИЗ 3.0.0.0

--- StD_Player_3/Properties/AssemblyInfo.cs
+++ StD_Player_3/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("StD_Player_3")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Плеер театра «СтД»")]
+[assembly: AssemblyDescription("Музыкальный плеер театра «Старый Дом» для получения независимых источников звука.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StD_Player_3")]
+[assembly: AssemblyCompany("Старый Дом")]
+[assembly: AssemblyProduct("StD _Player")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,7 @@ using System.Windows;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: NeutralResourcesLanguage("ru")]
+
