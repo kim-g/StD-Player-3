b9a8ad319cf155f28d1107447abbc6543cbf75b1
Григорий Ким <kim-g@yandex.ru>
2025-11-20 16:24:39 +0500

    Релиз 3.2.1 – обработка исключений
    Добавлен внешний обработчик исключений, который выдаёт окно, но не прекразает выполнение кода.
    
    Добавлено окно выдачи исключений.

--- StD_Player_3/App.xaml.cs
+++ StD_Player_3/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace StD_Player_3
 {
@@ -13,5 +14,44 @@ namespace StD_Player_3
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Перехват исключений в UI-потоке
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+
+            // Перехват исключений в других потоках
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            // Перехват исключений тасков
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            ShowErrorWindow(e.Exception);
+            e.Handled = true; // предотвращаем падение
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            ShowErrorWindow(e.ExceptionObject as Exception);
+        }
+
+        private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            ShowErrorWindow(e.Exception);
+            e.SetObserved();
+        }
+
+        private void ShowErrorWindow(Exception ex)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                var wnd = new ErrorWindow(ex);
+                wnd.ShowDialog();
+            });
+        }
     }
 }
---
+++ StD_Player_3/ErrorWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StD_Player_3
+{
+    /// <summary>
+    /// Логика взаимодействия для ErrorWindow.xaml
+    /// </summary>
+    public partial class ErrorWindow : Window
+    {
+        public string ErrorText { get; set; }
+
+        public ErrorWindow(Exception ex)
+        {
+            InitializeComponent();
+            ErrorText = ex.ToString();
+            DataContext = this;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
